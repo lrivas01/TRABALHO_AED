3cbcda3cda26c98a6d64fca4e77c9b256278dbd2
Hase505 <erickwinck5@gmail.com>
2025-06-23 19:32:54 -0300

    Adicionar documentação de funções de arquivos

--- src/arquivo.c
+++ src/arquivo.c
@@ -10,6 +10,20 @@
 #define NOME_ARQUIVO_LIVRO      "livro.dat"
 #define NOME_ARQUIVO_USUARIO    "usuario.dat"
 
+/*
+ * cria_lista_vazia - Inicializa um arquivo binário com uma lista encadeada vazia
+ *
+ * @arq - ponteiro para arquivo binário já aberto em modo leitura/escrita
+ *
+ * Pré-condições:
+ *	- O arquivo deve estar aberto com permissão de leitura e escrita
+ *	- O ponteiro arq deve ser válido (não nulo)
+ *
+ * Pós-condições:
+ *	- Um cabeçalho é escrito no início do arquivo, marcando a lista como vazia:
+ *	- Retorno 0 caso sucesso
+ *	- Retorno negativo caso erro
+ */
 int cria_lista_vazia(FILE *arq) {
         CABECALHO cab;
         cab.pos_cabeca = -1;
@@ -27,6 +41,18 @@ int cria_lista_vazia(FILE *arq) {
         return SUCESSO;
 }
 
+/*
+ * le_cabecalho - funcao que le o cabecalho do arquivo com as informacoes da lista
+ *
+ * @arq - ponteiro para arquivo binário aberto em modo leitura/escrita
+ *
+ * Pré-condições:
+ *	- O arquivo deve estar aberto e ser um arquivo da lista.
+ *
+ * Pós-condições:
+ *	- Retorna um ponteiro para a estrutura CABECALHO.
+ *	- Em caso de erro (falha em fseek, fread ou malloc), retorna NULL.
+ */
 CABECALHO* le_cabecalho(FILE *arq) {
         CABECALHO *cab = malloc(sizeof(CABECALHO));
         if (!cab) return NULL;
@@ -42,6 +68,21 @@ CABECALHO* le_cabecalho(FILE *arq) {
         return cab;
 }
 
+/*
+ * escreve_cabecalho - Sobrescreve o cabeçalho do arquivo binário com nova estrutura
+ *
+ * @arq - ponteiro para arquivo binário aberto em modo leitura/escrita
+ * @cab - ponteiro para estrutura CABECALHO com os dados atualizados
+ *
+ * Pré-condições:
+ *	- O arquivo deve estar aberto em modo leitura/escrita
+ *	- O ponteiro cab deve ser válido e previamente preenchido
+ *
+ * Pós-condições:
+ *	- O cabeçalho do arquivo é atualizado com os dados fornecidos
+ *	- Retorna 0  em caso de sucesso
+ *	- Retorna valor negativo caso ocorra erro
+ */
 int escreve_cabecalho(FILE* arq,CABECALHO* cab) {
         if (fseek(arq, 0, SEEK_SET) != 0)
                 return ERRO_ARQUIVO_SEEK;
@@ -52,7 +93,25 @@ int escreve_cabecalho(FILE* arq,CABECALHO* cab) {
         return SUCESSO;
 }
 
-int inicializar_arquivo(const char* caminho) {
+/*
+ * inicializar_arquivo - função interna que inicializa um arquivo binário com cabeçalho
+ *
+ * @caminho - caminho completo para o arquivo binário a ser inicializado
+ *
+ * Pré-condições:
+ *      - Caminho para o arquivo deve ser válido.
+ *      - Deve ser possível abrir ou criar o arquivo em modo leitura/escrita.
+ * Pós-condições:
+ *      - Caso o arquivo não exista, um arquivo binário é criado com a estrutura de cabeçalho.
+ *      - Caso o arquivo exista, verifica-se se possui um cabeçalho válido. Se não tiver, cria um.
+ *      - Caso o arquivo exista e tenha um cabeçalho válido, nada é feito.
+ *      - Retorna SUCESSO (0) em caso de sucesso.
+ *      - Retorna valores negativos em caso de erro:
+ *              - ERRO_ABRIR_ARQUIVO (-10): o arquivo não pôde ser aberto (falta de permissões ou diretório inválido).
+ *              - ERRO_ARQUIVO_SEEK (-1): não foi possível fazer posicionamento no arquivo (erro no fseek).
+ *              - ERRO_CRIAR_LISTA (-22): não foi possível criar um cabeçalho novo no arquivo.
+ */
+static int inicializar_arquivo(const char* caminho) {
         FILE* arquivo = fopen(caminho, "a+b");  // cria se não existir
         if (arquivo == NULL) 
                 return ERRO_ABRIR_ARQUIVO;
@@ -77,6 +136,29 @@ int inicializar_arquivo(const char* caminho) {
         return SUCESSO;
 }
 
+/*
+ * inicializar_base_de_dados - inicializa arquivos binários de usuários, livros e empréstimos
+ *
+ * @caminho_diretório - diretório que os arquivos binários serão armazenados
+ *
+ * Essa função recebe um diretório, podendo utilizar separador '/' em ambientes Linux
+ * ou '\' e '/' em ambientes Windows. Ela procura no diretório pelos arquivos binários e, 
+ * se não existirem ou existirem sem cabeçalhos válidos, eles são criados. Caso existam,
+ * nada é feito.
+ *
+ * Pré-condições:
+ *	- O diretório deve existir e ser válido.
+ *	- Em ambientes linux, o diretório deve estar no formato "/caminho/para/diretorio" ou "/caminho/para/diretorio/".
+ *	- Em ambientes windows, além da forma linux-like, o diretório pode estar no formato "\caminho\para\diretorio" ou
+ *		"\caminho\para\diretorio\".
+ *	- O diretório deve possuir permissões de leitura e escrita.
+ * Pós-condições:
+ *	- Os arquivos binários para listas encadeadas são criados e inicializados com cabeçalho, caso não existam.
+ *	- Se os arquivos existirem e estarem inicializados, a função não faz nada.
+ *	- Retorna SUCESSO (0) em caso de sucesso.
+ *	- Retorna valores negativos em caso de erro:
+ *		- ERRO_INICIALIZAR_ARQUIVO (-22): caso algum arquivo não consiga ser inicializado.
+ */
 int inicializar_base_de_dados(char *caminho_diretorio) {
         char caminho_base[TAM_MAX_CAMINHO];
         strncpy(caminho_base, caminho_diretorio, TAM_MAX_CAMINHO - 1);
